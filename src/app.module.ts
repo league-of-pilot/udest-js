95952c218041b0194a9fdc0f8733391ccb275162
Danh Tran <danh2k@nexondv.com>
2024-05-29 11:39:33 +0700

    refac: remove unused app co and se

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { MessagesModule } from './messages/messages.module'
 
 @Module({
   imports: [MessagesModule],
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
