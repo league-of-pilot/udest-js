9db21fb6252a2ff5d6ee9c444d2c51feff7d928c
Danh Tran <danh2k@nexondv.com>
2024-06-20 15:54:47 +0700

    init type orm forRoot

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common'
 import { MessagesModule } from './messages/messages.module'
 import { UsersModule } from './users/users.module'
 import { ReportsModule } from './reports/reports.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [UsersModule, ReportsModule, MessagesModule],
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: '_ignore_mock/db.sqlite',
+      entities: [],
+      synchronize: true
+    })
+  ],
   controllers: [],
   providers: []
 })
