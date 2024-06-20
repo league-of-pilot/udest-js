20fa0dbffc4faf375aaea00ee79eacc0879c85ca
Danh Tran <danh2k@nexondv.com>
2024-06-20 15:54:11 +0700

    nest g res [module]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { MessagesModule } from './messages/messages.module'
+import { UsersModule } from './users/users.module'
+import { ReportsModule } from './reports/reports.module'
 
 @Module({
-  imports: [MessagesModule],
+  imports: [UsersModule, ReportsModule, MessagesModule],
   controllers: [],
   providers: []
 })
