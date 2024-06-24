e0f1cc3a960292c0e0e1694c7057de3a88294ae8
Danh Tran <danh2k@nexondv.com>
2024-06-24 14:09:32 +0700

    applying middleware in module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,22 +1,37 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { UsersController } from './users.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './user.entity'
 import { AuthService } from './auth.service'
-import { CurrentUserInterceptor } from './current-user.interceptor'
-import { APP_INTERCEPTOR } from '@nestjs/core'
+import { CurrentUserMiddleware } from './current-user.middleware'
+import { MessagesController } from 'src/messages/messages.controller'
+// import { MessagesModule } from 'src/messages/messages.module'
+// import { CurrentUserInterceptor } from './current-user.interceptor'
+// import { APP_INTERCEPTOR } from '@nestjs/core'
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [
     UsersService,
-    AuthService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor
-    }
+    AuthService
+    // {
+    //   provide: APP_INTERCEPTOR,
+    //   useClass: CurrentUserInterceptor
+    // }
   ]
 })
-export class UsersModule {}
+
+// Tuy nhiên cần phải đảm bảo currentUser middleware chạy sau seassion middleware
+// https://docs.nestjs.com/middleware#middleware-consumer
+// https://docs.nestjs.com/middleware#applying-middleware
+// Apply như vậy ko hay lắm vì phải import MessagesController vào đây
+// TODO - check lại logic, nếu đẩy lên app thì cần đảm bảo thứ tự chạy
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CurrentUserMiddleware)
+      .forRoutes(UsersController, MessagesController)
+  }
+}
