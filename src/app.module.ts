4293335132cd2430247b50b0e293c11dfe1d4370
Danh Tran <danh2k@nexondv.com>
2024-06-24 11:58:58 +0700

    FEAT: setup nest config with forRootASync, cross-env script

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,37 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './users/user.entity'
 import { Report } from './reports/reports.entity'
 import { DemosModule } from './demos/demos.module'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { join } from 'path'
 
 @Module({
   imports: [
     UsersModule,
     ReportsModule,
     MessagesModule,
-    // https://thriveread.com/nestjs-typeorm-sqlite/
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: '_ignore_mock/db.sqlite',
-      entities: [User, Report],
-      synchronize: true
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
     }),
+    // https://docs.nestjs.com/fundamentals/dynamic-modules#custom-options-factory-class
+    TypeOrmModule.forRootAsync({
+      // https://docs.nestjs.com/fundamentals/custom-providers#factory-providers-usefactory
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'sqlite',
+        database: join('_ignore_mock', configService.get('DB_NAME')),
+        entities: [User, Report],
+        synchronize: true
+      })
+    }),
+    // https://thriveread.com/nestjs-typeorm-sqlite/
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: '_ignore_mock/db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true
+    // }),
+
     DemosModule
   ],
   controllers: [],
